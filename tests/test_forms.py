f055220b9dc67a798e7c2c9b9fd5e1fd7fb17f0e
Vitya Endless <dvitya031190@gmail.com>
2025-01-27 22:53:39 +0600

    added checks practice form v.final

--- tests/test_forms.py
+++ tests/test_forms.py
@@ -2,6 +2,8 @@ import time
 
 import allure
 import pytest
+import os
+from datetime import datetime
 
 from auxiliary.constants import HTMLEl, PseudoClsEl
 
@@ -150,16 +152,57 @@ class TestForm:
             for subject in practice_form_user_data['subjects']:
                 controller.practice_form_page.type(controller.practice_form_page.subjects, subject)
                 controller.practice_form_page.press(controller.practice_form_page.subjects)
-        # with allure.step('Нажать кнопку отправки формы'):
-        #     controller.practice_form_page.click(controller.practice_form_page.submit)
-        # with allure.step('Проверка результата введенных данных'):
-        #     controller.helper.to_contain_text(
-        #         controller.practice_form_page.name_result,
-        #         practice_form_user_data['first_name'] + " " + practice_form_user_data['last_name']
-        #     )
-        #     table_gender = controller.practice_form_page.get_element_text(controller.practice_form_page.gender_result)
-        #     controller.helper.is_eq(choised_gender, table_gender)
-        #     controller.helper.to_contain_text(
-        #         controller.practice_form_page.phone_result,
-        #         practice_form_user_data['phone']
-        #     )
+            # controller.practice_form_page.click(controller.practice_form_page.hobby_music)
+            # controller.practice_form_page.click(controller.practice_form_page.hobby_reading)
+            controller.practice_form_page.click(controller.practice_form_page.hobby_sports)
+            hobbies = []
+            # hobbies.append(controller.practice_form_page.get_element_text(controller.practice_form_page.hobby_music))
+            # hobbies.append(controller.practice_form_page.get_element_text(controller.practice_form_page.hobby_reading))
+            hobbies.append(controller.practice_form_page.get_element_text(controller.practice_form_page.hobby_sports))
+            current_working_dir = os.getcwd()
+            file_path = os.path.join(current_working_dir, f"uploads/{practice_form_user_data['picture']}")
+            controller.practice_form_page.page.set_input_files(
+                controller.practice_form_page.upload_picture['selector'], file_path
+            )
+            controller.practice_form_page.fill(
+                controller.practice_form_page.current_address, practice_form_user_data['address']
+            )
+            controller.practice_form_page.type(controller.practice_form_page.state, practice_form_user_data['state'])
+            controller.practice_form_page.press(controller.practice_form_page.state)
+            controller.practice_form_page.type(controller.practice_form_page.city, practice_form_user_data['city'])
+            controller.practice_form_page.press(controller.practice_form_page.city)
+        with allure.step('Нажать кнопку отправки формы'):
+            controller.practice_form_page.click(controller.practice_form_page.submit)
+        with allure.step('Проверка результата введенных данных'):
+            controller.helper.to_contain_text(
+                controller.practice_form_page.name_result,
+                f"{practice_form_user_data['first_name']} {practice_form_user_data['last_name']}"
+            )
+            controller.helper.to_contain_text(
+                controller.practice_form_page.email_result, practice_form_user_data['email']
+            )
+            table_gender = controller.practice_form_page.get_element_text(controller.practice_form_page.gender_result)
+            controller.helper.is_eq(choised_gender, table_gender)
+            controller.helper.to_contain_text(
+                controller.practice_form_page.phone_result, practice_form_user_data['phone']
+            )
+            birth_d_in = datetime.strptime(practice_form_user_data['birth_date'], '%d %b %Y').strftime('%Y-%m-%d')
+            birth_d_out = datetime.strptime(
+                controller.practice_form_page.get_element_text(controller.practice_form_page.birth_date_result),
+                '%d %B,%Y'
+            ).strftime('%Y-%m-%d')
+            controller.helper.is_eq(birth_d_in, birth_d_out)
+            controller.helper.to_contain_text(
+                controller.practice_form_page.subjects_result, practice_form_user_data['subjects'][0]
+            )
+            controller.helper.to_contain_text(controller.practice_form_page.hobbies_result, hobbies[0])
+            controller.helper.to_contain_text(
+                controller.practice_form_page.picture_result, practice_form_user_data['picture']
+            )
+            controller.helper.to_contain_text(
+                controller.practice_form_page.current_address_result, practice_form_user_data['address']
+            )
+            controller.helper.to_contain_text(
+                controller.practice_form_page.state_city_result,
+                f"{practice_form_user_data['state']} {practice_form_user_data['city']}"
+            )
